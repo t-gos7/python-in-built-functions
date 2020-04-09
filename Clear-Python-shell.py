72f988e9865481b80a1aedf2b213c92f8b5d00f8
Tarit Goswami <tg2063@cse.jgec.ac.in>
2020-04-09 18:13:13 +0530

    Update Clear-Python-shell.py

--- Clear-Python-shell.py
+++ Clear-Python-shell.py
@@ -7,15 +7,15 @@ This is a system independent function to clear Python shell.
 
 def clear():
   '''Function to clear Python shell, independent of system'''
-  yn = input('Are you sure? Y/N\n  ')
+  yn = input('Everything you have written in this shell will be removed, Are you sure? Y/N\n  ')
   if yn == 'Y' or yn == 'y':
-    import os
-    from subprocess import call
     from time import sleep
     if os.name == 'posix':
+      from subprocess import call
       sleep(3)
       _ = call('clear', shell=True)
     elif os.name == 'nt':
+      import os
       sleep(3)
       _ = os.system('CLS')
 clear()
